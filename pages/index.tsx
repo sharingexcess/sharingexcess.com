de3e060b7eee4f5a8c5d24e6290cda6c92859fc0
Ryan McHenry <rmchenry@izotope.com>
2021-11-29 23:32:20 -0500

    v1.0.12

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,8 @@ import {
 } from 'components/Home'
 import { Header, Footer } from 'components'
 import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 const Home: NextPage = () => {
-  const router = useRouter()
-
-  useEffect(() => {
-    if (router.pathname !== '/') {
-      router.push(router.pathname)
-    }
-  }, [router])
   return (
     <div id="Home">
       <Header />
