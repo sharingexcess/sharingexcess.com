45d498eabe7c2b13532545d4319f262b7fcbefe3
Luke Shin <ejs364@drexel.edu>
2021-12-15 16:03:45 -0500

    updated weekly numbers from Data

--- components/Home/Impact/Impact.tsx
+++ components/Home/Impact/Impact.tsx
@@ -7,20 +7,20 @@ import { useIsMobile } from 'hooks'
 const content = [
   {
     image: '/icons/food.png',
-    header: 5314283,
+    header: 5502387,
     subheader: 'Pounds of Food Rescued since 2018',
     body: 'Partnering with grocery stores, restaurants, wholesalers, and farmers, Sharing Excess has rescued and delivered food to over 180 nonprofits and food banks.',
   },
   {
     image: '/icons/money.png',
-    header: 10571678,
+    header: 10734934,
     prefix: '$',
     subheader: 'Total Retail Value of Rescued Food',
     body: 'With an average retail value of $2.86 per pound, Sharing Excess has returned over $9 million of valuable, fresh food to the local economy and community.',
   },
   {
     image: '/icons/cloud.png',
-    header: 19450275,
+    header: 20138736,
     subheader: 'Pounds of CO2 Diverted from Landfills',
     body: 'By keeping food waste out of landfills, Sharing Excess has diverted over 18 million pounds of carbon dioxide from the atmosphere.',
   },
