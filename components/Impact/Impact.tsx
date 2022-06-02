559c2f8eb24ebf0c2450e093d2fa474ae1bf8108
jiitra <74521649+jiitra@users.noreply.github.com>
2022-06-02 16:55:07 -0400

    fun

--- components/Impact/Impact.tsx
+++ components/Impact/Impact.tsx
@@ -1,4 +1,4 @@
-import { Spacer } from '@sharingexcess/designsystem'
+import { Button, Spacer } from '@sharingexcess/designsystem'
 import { PageHeader } from 'components/PageHeader/PageHeader'
 import { FlexContainer, Image, Text } from 'designsystem'
 import { useIsMobile } from 'hooks'
@@ -100,12 +100,52 @@ export const Impact: FC = () => {
           </Text>
         </FlexContainer>
       </div>
-      <Spacer height={96} />
+      <Spacer height={124} />
       <Image
         src="/impact/cumulative-impact.png"
         alt="Diagram"
         id="cumulative-impact"
       />
+
+      <Spacer height={124} />
+      <FlexContainer direction="vertical" id="Impact-annual-report-section">
+        <Text type="small-header" align="center">
+          Annual Report
+        </Text>
+        <Spacer height={22} />
+        <Text type="paragraph" align="center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          vulputate libero et velit interdum, ac aliquet odio mattis. Class
+          aptent taciti sociosqu ad litora torquent per conubia
+        </Text>
+        <Spacer height={22} />
+        <Button type="primary" size="medium" color="green">
+          Full Report
+        </Button>
+      </FlexContainer>
+
+      <Spacer height={124} />
+      <FlexContainer direction="vertical" id="Impact-metric-section">
+        <Text type="primary-header" align="center">
+          $1 = 16 meals served
+        </Text>
+        <Spacer height={22} />
+        <Text type="paragraph" align="center">
+          Every dollar donated contributes to this metric. One dollar goes to 20
+          pounds of food delivered, $57.20 in food value, and 73.2 pounds of
+          greenhouse gases reduced.
+        </Text>
+        <Spacer height={32} />
+        <Image
+          src="/impact/drexel-popup.jpg"
+          alt="Food distribution at Drexel University"
+          id="Impact-metric-section-img"
+        />
+        <Spacer height={22} />
+        <Button type="primary" size="large" color="green">
+          Start A Donation
+        </Button>
+      </FlexContainer>
     </div>
   )
 }
